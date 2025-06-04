77a8c833bfa3a748f3e8deb42f8b18bc12d79491
victorm-lc <victor@langchain.dev>
2025-06-04 17:05:29 -0400

    update no config

--- src/react_agent/graph_without_config.py
+++ src/react_agent/graph_without_config.py
@@ -1,7 +1,3 @@
-"""Define a custom Reasoning and Action agent.
-
-Works with a chat model with tool calling support.
-"""
 from src.react_agent.tools import basic_research_tool, get_todays_date
 from langgraph.prebuilt import create_react_agent
 from src.utils import load_chat_model
@@ -11,7 +7,11 @@ async def make_graph():
     # initialize our model and tools    
     llm = load_chat_model("openai/gpt-4.1-mini")
     tools = [basic_research_tool, get_todays_date]
-    prompt = "You are a helpful AI assistant!"
+    prompt = """
+        You are a helpful AI assistant trained in creating engaging social media content!
+        you have access to two tools: basic_research_tool and get_todays_date. Please get_todays_date then 
+        perform any research if needed, before generating a social media post.
+        """
 
     # Compile the builder into an executable graph
     graph = create_react_agent(
